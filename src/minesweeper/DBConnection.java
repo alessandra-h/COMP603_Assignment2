95085de5cab30299efdb9ade8d35a54260e238d5
Alessandra <Alessandra@192.168.1.12>
2022-06-02 16:08:34 +1200

    Beginning database connection

---
+++ src/minesweeper/DBConnection.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Alessandra
+ */
+public class DBConnection {
+
+    private static final String USERNAME = "minesweeper";
+    private static final String PASSWORD = "minesweeper";
+    private static final String URL = "jdbc:derby:/MinesweeperDB; create=true";
+
+    Connection connection;
+
+    public DBConnection() {
+        makeConnection();
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    private void makeConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                System.out.println(URL + " has been connected!");
+            } catch (SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+    }
+
+    private void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        DBConnection db = new DBConnection();
+        
+    }
+}
